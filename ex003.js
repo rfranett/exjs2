6e7d5a66f41c046b17c122b00731566bc160fd93
rfranett <rfranett@gmail.com>
2022-12-05 23:36:35 -0300

    lista de meses do ano

---
+++ ex003.js
@@ -0,0 +1,9 @@
+
+let mesesDoAno =['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro',]
+
+document.write(`<div class="meses">
+                <h2> Array de Meses do Ano</h2>
+                <p>${mesesDoAno[0]}, ${mesesDoAno[1]}, ${mesesDoAno[2]},
+                </p> ${mesesDoAno[3]}, ${mesesDoAno[4]}, ${mesesDoAno[5]},
+                <p> ${mesesDoAno[6]}, ${mesesDoAno[7]}, ${mesesDoAno[8]},
+                </p><p> ${mesesDoAno[9]}, ${mesesDoAno[10]}, ${mesesDoAno[11]},</p></div>`)
