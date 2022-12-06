f4a13f9a4a288b9ab36e7accb1390a48b7717762
rfranett <rfranett@gmail.com>
2022-12-06 16:09:21 -0300

    campo novo para alterar frases

--- ex003.js
+++ ex003.js
@@ -3,7 +3,15 @@ let mesesDoAno =['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Ju
 
 document.write(`<div class="meses">
                 <h2> Array de Meses do Ano</h2>
-                <p>${mesesDoAno[0]}, ${mesesDoAno[1]}, ${mesesDoAno[2]},
-                </p> ${mesesDoAno[3]}, ${mesesDoAno[4]}, ${mesesDoAno[5]},
-                <p> ${mesesDoAno[6]}, ${mesesDoAno[7]}, ${mesesDoAno[8]},
-                </p><p> ${mesesDoAno[9]}, ${mesesDoAno[10]}, ${mesesDoAno[11]},</p></div>`)
+                <p>${mesesDoAno[0]}, ${mesesDoAno[1]}, ${mesesDoAno[2]}, ${mesesDoAno[3]}, ${mesesDoAno[4]}, ${mesesDoAno[5]}, ${mesesDoAno[6]}, ${mesesDoAno[7]}, ${mesesDoAno[8]}, ${mesesDoAno[9]}, ${mesesDoAno[10]}, ${mesesDoAno[11]},</div>`)
+
+
+
+                
+let dis =['espero que esteja tudo bem', 'lindos frutos e verdes pastos','felizes e graciosos','que seus dias sejam cada vez mais iluminados','felizes',]
+
+
+document.write(`<div class="dis">
+                <h2>embaralhando frases</h2>
+                <p>${dis[0]}, ${mesesDoAno[1]}, ${dis[2]}, ${mesesDoAno[3]}, ${mesesDoAno[4]},${dis[1]}, ${mesesDoAno[5]},${mesesDoAno[6]},${dis[0]}, ${mesesDoAno[7]}, ${mesesDoAno[8]},${mesesDoAno[9]}, ${mesesDoAno[10]}, ${mesesDoAno[11]},${dis[4]},</div>`)
+
